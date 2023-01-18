32f679ea4d232dbae2fb3abf7463883d077c25f9
YaffetK <yaffet97@gmail.com>
2023-01-18 18:31:05 +0100

    Add some dum,y data to work with

--- rentingapp/pages/index.jsx
+++ rentingapp/pages/index.jsx
@@ -1,12 +1,29 @@
 import Navbar from "./components/Navbar";
+import Image from "next/image";
+import data from '../data.json'
 
 
 const Home = () => {
-  return <main>
-    <Navbar />
-    <h1 className="text-center text-lblue">Hello World</h1>
+  return (
+    <main className="min-h-screen">
+      <Navbar />
 
-  </main>;
+      <div className="flex w-full flex-col items-center mt-12 flex-grow min-h-fit  ">
+
+        <Image
+          className="rounded-full"
+          alt={data.name}
+          src={data.avatar}
+          width={120}
+          height={120}
+        />
+        <h1 className="font-semibold px-4 pt-2">{data.name}</h1>
+
+      </div>
+
+
+    </main>
+  );
 }
 
 export default Home
