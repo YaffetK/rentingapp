472be5f46c0520e2c63e8d56e38771cf1354a368
YaffetK <yaffet97@gmail.com>
2023-01-19 00:57:08 +0100

    WIP

--- frontend/rentingapp/.next/static/chunks/webpack.js
+++ frontend/rentingapp/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b922e964bf1369bb"; }
+/******/ 		__webpack_require__.h = function() { return "ef9f8401450f60da"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
