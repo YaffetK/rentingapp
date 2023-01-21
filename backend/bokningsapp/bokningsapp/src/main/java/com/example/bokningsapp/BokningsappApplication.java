84bca174cea59751cc7a53704b17f53d16f683e5
Luliya Masfun <97747241+LuliyaMasfun@users.noreply.github.com>
2023-01-21 17:45:47 +0100

    Revert "wip"

---
+++ backend/bokningsapp/bokningsapp/src/main/java/com/example/bokningsapp/BokningsappApplication.java
@@ -0,0 +1,13 @@
+package com.example.bokningsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BokningsappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BokningsappApplication.class, args);
+	}
+
+}
---
+++ backend/bokningsapp/bokningsapp/src/main/java/com/example/bokningsapp/service/RoleServiceImpl.java
@@ -0,0 +1,4 @@
+package com.example.bokningsapp.service;
+
+public class RoleServiceImpl {
+}
