198a7e9b2f93d8ca679efd4a2c0fa6918dd96084
lul1ya <luliamasfun1@gmail.com>
2023-01-24 16:00:13 +0100

    testing branch new frontend

--- frontend/rentingapp/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ["pbs.twimg.com", "mdbcdn.b-cdn.net"],
-  },
-};
-
-module.exports = nextConfig;
--- frontend/rentingapp/pages/equipment/index.jsx
+++
@@ -1,8 +0,0 @@
-
-const EquipmentDefault = () => {
-
-    return (
-        <div> This is the default Equipment Page</div>
-    )
-}
-export default EquipmentDefault
